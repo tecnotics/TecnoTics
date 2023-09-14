dc5b3da47ee66761accf04f4573b36b9a52b2a34
DaniLeon <144843516+tecnotics@users.noreply.github.com>
2023-09-14 10:19:01 -0500

    Mejora General

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,58 +1,96 @@
-
-import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import { useState } from "react";
+import {
+  AppBar,
+  Button,
+  Toolbar,
+  Typography,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import { Link } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function Navbar() {
-  const navbarStyle = {
-    flexGrow: 1,
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = (open) => () => {
+    setDrawerOpen(open);
   };
 
-  const titleStyle = {
-    flexGrow: 1,
-  };
-
-  const buttonStyle = {
-    marginLeft: "20px",
-  };
+  const menuItems = [
+    { text: "Inicio", link: "/" },
+    { text: "Outsourcing", link: "/acerca" },
+    { text: "Servicios", link: "/servicios" },
+    { text: "Soluciones", link: "/soluciones" },
+  ];
 
   return (
-    <div style={navbarStyle}>
-      <AppBar position="static">
+    <div>
+      <AppBar position="static" sx={{ backgroundColor: "#007bff" }}>
         <Toolbar>
-          <Typography variant="h6" style={titleStyle}>
-            TEST
+          <Typography variant="h6" sx={{ flexGrow: 1, color: "white" }}>
+            TecnoTics
           </Typography>
-          <Button color="inherit" style={buttonStyle} component={Link} to="/">
-            Inicio
-          </Button>
-          <Button
-            color="inherit"
-            style={buttonStyle}
-            component={Link}
-            to="/acerca"
-          >
-            Acerca de
-          </Button>
-          <Button
-            color="inherit"
-            style={buttonStyle}
-            component={Link}
-            to="/servicios"
-          >
-            Servicios
-          </Button>
-          <Button
-            color="inherit"
-            style={buttonStyle}
-            component={Link}
-            to="/contacto"
-          >
-            Contacto
-          </Button>
+          {isMobile ? (
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={toggleDrawer(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+          ) : (
+            <div className="desktop-menu">
+              {menuItems.map((item, index) => (
+                <Button
+                  key={index}
+                  color="inherit"
+                  component={Link}
+                  to={item.link}
+                  sx={{ color: "white" }}
+                >
+                  {item.text}
+                </Button>
+              ))}
+            </div>
+          )}
         </Toolbar>
       </AppBar>
+      {isMobile && (
+        <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+          <div
+            sx={{ width: 250, backgroundColor: "#007bff" }}
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+          >
+            <List>
+              {menuItems.map((item, index) => (
+                <ListItem
+                  button
+                  key={index}
+                  component={Link}
+                  to={item.link}
+                  onClick={toggleDrawer(false)}
+                >
+                  <ListItemText primary={item.text} />
+                </ListItem>
+              ))}
+            </List>
+          </div>
+        </Drawer>
+      )}
     </div>
   );
 }
 
 export default Navbar;
+
--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -1,8 +1,10 @@
+import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import Servicio from "../../Components/Servicio/Servicio";
 import Footer from "../../Components/Footer/Footer";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button"; 
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
 import "./Home.css";
 
 function Home() {
@@ -15,34 +17,70 @@ function Home() {
           <Typography variant="subtitle1">Soluciones Corporativas</Typography>
         </section>
         <section className="banner-bienvenida">
-          <div className="banner-content">
-            <Typography variant="h2">Bienvenido a nuestro sitio web</Typography>
-            <Typography variant="body1">
-              Descubre cómo podemos ayudarte.
-            </Typography>
-            <Button variant="contained" color="primary" href="#servicios">
-              Ver Servicios
-            </Button>
-          </div>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <div className="banner-content">
+                <Typography variant="h2">¡Bienvenido a TecnoTics!</Typography>
+                <Typography variant="body2">
+                  Estamos aquí para impulsar tu éxito en el mundo digital.
+                  Descubre cómo podemos ayudarte a transformar tu negocio.
+                </Typography>
+                <Button variant="contained" color="primary" href="#servicios">
+                  Ver Servicios
+                </Button>
+              </div>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/TBpRnnqyy5A?si=aiHIRr1EOPTQqLGk"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+              ></iframe>
+            </Grid>
+          </Grid>
         </section>
         <section className="servicios" id="servicios">
           <div className="servicios-content">
-            <Typography variant="h2">Nuestros Servicios</Typography>
-            <Servicio
-              iconClass="fas fa-cogs"
-              titulo="Servicio 1"
-              descripcion="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-            />
-            <Servicio
-              iconClass="fas fa-chart-bar"
-              titulo="Servicio 2"
-              descripcion="Nullam vitae tortor vel urna lacinia venenatis."
-            />
-            <Servicio
-              iconClass="fas fa-globe"
-              titulo="Servicio 3"
-              descripcion="Curabitur et odio in libero viverra venenatis."
-            />
+            <Typography variant="h3">Nuestros Servicios</Typography>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={4}>
+                <Servicio
+                  iconClass="fas fa-cogs"
+                  titulo="Servicios DNS"
+                  descripcion="Gestionamos y optimizamos tus registros DNS para garantizar un acceso rápido y seguro a tus servicios en línea. Nuestro equipo de expertos se encarga de configurar, monitorear y mantener tus registros DNS, asegurando la disponibilidad y la seguridad de tu presencia en línea."
+                />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Servicio
+                  iconClass="fas fa-chart-bar"
+                  titulo="Outsourcing"
+                  descripcion="Nuestro servicio de outsourcing va más allá de la mera externalización; actuamos como una extensión de su equipo, asegurando que cada aspecto tecnológico de su negocio opere a la máxima eficiencia.
+"
+                />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Servicio
+                  iconClass="fas fa-globe"
+                  titulo="Office 365"
+                  descripcion="En TecnoTics, entendemos que la productividad es esencial para el éxito empresarial. Es por eso que ofrecemos Office 365, una solución integral de productividad y colaboración diseñada para impulsar la eficiencia de tu empresa."
+                />
+              </Grid>
+            </Grid>
+          </div>
+        </section>
+        <section className="contacto" id="contacto">
+          <div className="contacto-content">
+            <Typography variant="h2">Contáctanos</Typography>
+            <Typography variant="body2">
+              ¿Listo para comenzar tu proyecto? ¡Hablemos!
+            </Typography>
+            <Button variant="contained" color="primary" href="#contacto">
+              Contactar
+            </Button>
           </div>
         </section>
       </div>
