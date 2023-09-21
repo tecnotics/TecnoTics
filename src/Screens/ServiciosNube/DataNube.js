9941100ad1561f7e8a5be1d7fc8c2b8e9ae2e3fe
DaniLeon <144843516+tecnotics@users.noreply.github.com>
2023-09-21 11:01:17 -0500

    prueba imagen

--- src/Screens/ServiciosNube/DataNube.js
+++ src/Screens/ServiciosNube/DataNube.js
@@ -1,3 +1,5 @@
+import chip from "../../../src/assets/images/chip.jpg"
+
 export const tecnoTicsServices = {
   title: "Nuestros Servicios",
   services: [
@@ -5,42 +7,42 @@ export const tecnoTicsServices = {
       title: "Servicios en la Nube",
       description:
         "Alquiler de Servidores, Filtrado de Internet y Firewall de DNS, Telefonía IP en la Nube.",
-      imageUrl: "../../../src/assets/images/chip.jpg", // Ruta de la imagen
-      moreLink: "/ver-mas-servicio-nube", // Enlace para "Ver más"
+      imageUrl: {chip},
+      moreLink: "/ver-mas-servicio-nube", 
     },
     {
       title: "Servicios Cloud",
       description:
         "Infraestructura de TI sólida y confiable para empresas en servidores en la nube.",
-      imageUrl: "../../../src/assets/images/chip.jpg",
+      imageUrl:{chip},
       moreLink: "/ver-mas-servicio-cloud",
     },
     {
       title: "Servicios DNS",
       description:
         "Gestión, optimización y seguridad de registros DNS para servicios en línea.",
-      imageUrl: "../../../src/assets/images/chip.jpg",
+      imageUrl: {chip},
       moreLink: "/ver-mas-servicio-dns",
     },
     {
       title: "Telefonía IP",
       description:
         "Modernización de comunicaciones empresariales con telefonía IP en la nube.",
-      imageUrl: "../../../src/assets/images/chip.jpg",
+      imageUrl: {chip},
       moreLink: "/ver-mas-servicio-telefonia",
     },
     {
       title: "Servicios VoIP",
       description:
         "Portal de usuario, panel administrativo, panel de llamadas, grabación de llamadas y números internacionales para mejorar las comunicaciones empresariales.",
-      imageUrl: "../../../src/assets/images/chip.jpg",
+      imageUrl: {chip},
       moreLink: "/ver-mas-servicio-voip",
     },
     {
       title: "Seguridad en la Nube",
       description:
         "Protección avanzada de aplicaciones y datos en línea, incluyendo firewall, detección de intrusiones y cifrado de datos.",
-      imageUrl: "../../../src/assets/images/chip.jpg",
+      imageUrl: {chip},
       moreLink: "/ver-mas-servicio-seguridad",
     },
     {
