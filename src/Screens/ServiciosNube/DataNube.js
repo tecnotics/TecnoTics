f8963ba1d4e9f8885890f8d476132b1a4ac901d5
DaniLeon <desarrollo@tecnotics.com>
2023-09-22 14:59:42 -0500

    Tabs en Servicios Nube

--- src/Screens/ServiciosNube/DataNube.js
+++ src/Screens/ServiciosNube/DataNube.js
@@ -1,4 +1,61 @@
 import chip from "../../assets/images/chip.jpg";
+import servicionube from "../../assets/images/serviciosnube.jpg";
+import serviciocloud from "../../assets/images/servicioscloud.jpg";
+
+export const acronisServices = {
+  title: "Acronis",
+  services: [
+    {
+      title: "Proteccion frente al ransomware",
+      description:
+        "Soluciones de Acronis para detener el ransomware, incluso variantes desconocidas.",
+      imageUrl: chip,
+      moreLink: "/ver-mas-servicio-ransomware",
+    },
+    {
+      title: "Administrcion de parches para empresas",
+      description:
+        "Soluciones de administración automatizada de parches de Acronis para proteger contra amenazas.",
+      imageUrl: chip,
+      moreLink: "/ver-mas-servicio-parches",
+    },
+    {
+      title: "Antimalware para entornos corporativos",
+      description:
+        "Soluciones antimalware de Acronis para neutralizar malware conocido y desconocido.",
+      imageUrl: chip,
+      moreLink: "/ver-mas-servicio-antimalware",
+    },
+    {
+      title: "Recuperacion ante desastres de TI",
+      description:
+        "Opciones de recuperación de talla mundial para evitar interrupciones empresariales.",
+      imageUrl: chip,
+      moreLink: "/ver-mas-servicio-ti",
+    },
+    {
+      title: "Copia de seguridad y recuperación de archivos",
+      description:
+        "Protección de datos incomparable con soluciones de copia de seguridad y recuperación de Acronis.",
+      imageUrl: chip,
+      moreLink: "/ver-mas-servicio-seguridad",
+    },
+    {
+      title: "Prevención a perdida de datos (DLP)",
+      description:
+        "Funciones de prevención de la fuga de datos (DLP) para bloquear transferencias no autorizadas.",
+      imageUrl: chip,
+      moreLink: "/ver-mas-servicio-dlp",
+    },
+    {
+      title: "Sector público y Administracion (Acronis SCS)",
+      description: "Copia de seguridad reforzada certificada por DoDIN APL.",
+      imageUrl: chip,
+      moreLink: "/ver-mas-servicio-scs",
+    },
+  ],
+};
+
 
 export const tecnoTicsServices = {
   title: "Nuestros Servicios",
@@ -7,14 +64,14 @@ export const tecnoTicsServices = {
       title: "Servicios en la Nube",
       description:
         "Alquiler de Servidores, Filtrado de Internet y Firewall de DNS, Telefonía IP en la Nube.",
-      imageUrl: chip,
+      imageUrl: servicionube,
       moreLink: "/ver-mas-servicio-nube",
     },
     {
       title: "Servicios Cloud",
       description:
         "Infraestructura de TI sólida y confiable para empresas en servidores en la nube.",
-      imageUrl: chip,
+      imageUrl: serviciocloud,
       moreLink: "/ver-mas-servicio-cloud",
     },
     {
--- src/Screens/ServiciosNube/ServiciosNube.jsx
+++ src/Screens/ServiciosNube/ServiciosNube.jsx
@@ -1,3 +1,4 @@
+import  { useState } from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
 import {
@@ -8,60 +9,128 @@ import {
   Grid,
   Typography,
   Button,
+  Tabs,
+  Tab,
 } from "@mui/material";
-import { tecnoTicsServices } from "./DataNube";
+import { tecnoTicsServices, acronisServices } from "./DataNube";
 import { motion } from "framer-motion";
 import "./ServiciosNube.css";
 
 function ServiciosNube() {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabChange = (event, newValue) => {
+    setActiveTab(newValue);
+  };
+
   return (
     <>
       <Navbar />
       <br />
       <br />
-      <br />
-      <br />
       <Container className="container">
         <Typography variant="h3">
-          <b>{tecnoTicsServices.title}</b>
+          <b>Servicios</b>
         </Typography>
         <br />
-        <Grid container spacing={3}>
-          {tecnoTicsServices.services.map((service, index) => (
-            <Grid item xs={12} sm={6} md={6} lg={6} key={index}>
-              {console.log(service)}
-              <motion.div
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5, delay: index * 0.1 }}
-              >
-                <Card>
-                  <CardMedia
-                    component="img"
-                    alt={service.title}
-                    height="140"
-                    image={service.imageUrl}
-                  />
-                  <CardContent>
-                    <Typography variant="h5" component="div">
-                      {service.title}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary">
-                      {service.description}
-                    </Typography>
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      href={service.moreLink}
-                    >
-                      Ver más
-                    </Button>
-                  </CardContent>
-                </Card>
-              </motion.div>
-            </Grid>
-          ))}
-        </Grid>
+
+        {/* Pestañas para TecnoTics y ACRONIS */}
+        <Tabs
+          value={activeTab}
+          onChange={handleTabChange}
+          indicatorColor="primary"
+          textColor="primary"
+          centered
+        >
+          <Tab label="TecnoTics" />
+          <Tab label="ACRONIS" />
+        </Tabs>
+        <br />
+        <br />
+
+        {/* Contenido de la sección de TecnoTics */}
+        {activeTab === 0 && (
+          <Grid container spacing={3}>
+            {tecnoTicsServices.services.map((service, index) => (
+              <Grid item xs={12} sm={6} md={6} lg={6} key={index}>
+                <motion.div
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{
+                    opacity: 1,
+                    y: 0,
+                  }}
+                  transition={{ duration: 0.5, delay: index * 0.1 }}
+                >
+                  <Card>
+                    <CardMedia
+                      component="img"
+                      alt={service.title}
+                      height="140"
+                      image={service.imageUrl}
+                    />
+                    <CardContent>
+                      <Typography variant="h5" component="div">
+                        {service.title}
+                      </Typography>
+                      <Typography variant="body2" color="textSecondary">
+                        {service.description}
+                      </Typography>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        href={service.moreLink}
+                      >
+                        Ver más
+                      </Button>
+                    </CardContent>
+                  </Card>
+                </motion.div>
+              </Grid>
+            ))}
+          </Grid>
+        )}
+
+        {/* Contenido de la sección de ACRONIS */}
+        {activeTab === 1 && (
+          <Grid container spacing={3}>
+            {acronisServices.services.map((service, index) => (
+              <Grid item xs={12} sm={6} md={6} lg={6} key={index}>
+                <motion.div
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{
+                    opacity: 1,
+                    y: 0,
+                  }}
+                  transition={{ duration: 0.5, delay: index * 0.1 }}
+                >
+                  <Card>
+                    <CardMedia
+                      component="img"
+                      alt={service.title}
+                      height="140"
+                      image={service.imageUrl}
+                    />
+                    <CardContent>
+                      <Typography variant="h5" component="div">
+                        {service.title}
+                      </Typography>
+                      <Typography variant="body2" color="textSecondary">
+                        {service.description}
+                      </Typography>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        href={service.moreLink}
+                      >
+                        Ver más
+                      </Button>
+                    </CardContent>
+                  </Card>
+                </motion.div>
+              </Grid>
+            ))}
+          </Grid>
+        )}
       </Container>
       <Footer />
     </>
